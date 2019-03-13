3506ab1ddc9f698913c372c458734c196452725f
Yauheni Skibinski <stikler51@gmail.com>
2019-03-13 16:53:23 +0300

    feat: implement some functions

--- src/max-heap.js
+++ src/max-heap.js
@@ -2,11 +2,14 @@ const Node = require('./node');
 
 class MaxHeap {
 	constructor() {
-		
+		this.root = null;
+		this.parentNodes = [];
 	}
 
 	push(data, priority) {
-		
+	    var n = new Node(data, priority);
+		this.insertNode(n);
+		this.shiftNodeUp(n);
 	}
 
 	pop() {
@@ -26,15 +29,23 @@ class MaxHeap {
 	}
 
 	isEmpty() {
-		
+		if (this.root == null) {
+			return true;
+		}
+		return false;
 	}
 
 	clear() {
-		
+		this.root = null;
+		this.parentNodes = [];
 	}
 
 	insertNode(node) {
-		
+	    if (this.root == null) {
+			this.root = node;
+		} else {
+			this.parentNodes.push(node);
+		}
 	}
 
 	shiftNodeUp(node) {
--- src/node.js
+++ src/node.js
@@ -1,22 +1,42 @@
 class Node {
 	constructor(data, priority) {
-
+		this.data = data;
+		this.priority = priority;
+		this.parent = null;
+		this.left = null;
+		this.right = null;
 	}
 
 	appendChild(node) {
+		if (!this.left) {
+			this.left = node;
+		} else if (!this.right && this.left){
+			this.right = node;
+		} else {
+			return;
+		}
 
 	}
 
 	removeChild(node) {
-
+		if (this.left == node) {
+			this.left = null;
+		} else if (this.right == node) {
+			this.right = null;
+		} else {
+			throw new Error('error');
+		}
 	}
 
 	remove() {
-
+		if (!this.parent) {
+			return;
+		}
+		this.parent.removeChild(this);
 	}
 
 	swapWithParent() {
-		
+
 	}
 }
 
