70ae7919970102e0dac880834d94c28da9792415
Yauheni Skibinski <stikler51@gmail.com>
2019-03-15 17:50:36 +0300

    more test are passed

--- src/node.js
+++ src/node.js
@@ -10,8 +10,10 @@ class Node {
 	appendChild(node) {
 		if (!this.left) {
 			this.left = node;
+			node.parent = this;
 		} else if (!this.right && this.left){
 			this.right = node;
+            node.parent = this;
 		} else {
 			return;
 		}
@@ -21,8 +23,10 @@ class Node {
 	removeChild(node) {
 		if (this.left == node) {
 			this.left = null;
+			node.parent = null;
 		} else if (this.right == node) {
 			this.right = null;
+            node.parent = null;
 		} else {
 			throw new Error('error');
 		}
@@ -37,7 +41,39 @@ class Node {
 
 	swapWithParent() {
 
+
+	    if (!this.parent) {
+	        return;
+        } else if (this.parent.left == this){
+            let child = this;
+            let parent = this.parent;
+            let grandPa = parent.parent;
+            let childLeft = child.left;
+            let childRight = child.right;
+            let bro = parent.right;
+
+
+            parent.parent = this;
+            parent.left = childLeft;
+            parent.right = childRight;
+            child.parent = grandPa;
+
+            grandPa.
+            child.left = parent;
+        }
+
 	}
 }
 
 module.exports = Node;
+
+let a = new Node (1, 15);
+let b = new Node(2, 10);
+let c = new Node(3, 20);
+b.parent = a;
+c.parent = a;
+
+a.appendChild(b);
+a.appendChild(c);
+b.swapWithParent();
+console.log(b);
