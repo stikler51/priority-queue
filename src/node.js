6fdc5e42a1644b76a565cb0cbf49bf24d4d8d583
Yauheni Skibinski <stikler51@gmail.com>
2019-03-16 16:49:13 +0300

    all tests for Node class are pass

--- src/node.js
+++ src/node.js
@@ -40,40 +40,77 @@ class Node {
 	}
 
 	swapWithParent() {
-
-
 	    if (!this.parent) {
 	        return;
-        } else if (this.parent.left == this){
-            let child = this;
-            let parent = this.parent;
-            let grandPa = parent.parent;
-            let childLeft = child.left;
-            let childRight = child.right;
-            let bro = parent.right;
-
-
-            parent.parent = this;
-            parent.left = childLeft;
-            parent.right = childRight;
-            child.parent = grandPa;
-
-            grandPa.
-            child.left = parent;
         }
+	    const grandPa = this.parent.parent;
+	    const parent = this.parent;
+	    const leftC = this.left;
+	    const rightC = this.right;
+
+		parent.parent = this;
+		this.parent = grandPa;
+
+	    if (parent.left == this) {
+	    	this.right = parent.right;
+	    	this.left = parent;
+
+	    	if (parent.right) {
+	    		parent.right.parent = this;
+			}
+
+			parent.left = leftC;
+			parent.right = rightC;
+
+			if (grandPa && grandPa.right == parent) {
+				grandPa.right = this;
+			} else if (grandPa && grandPa.left == parent) {
+				grandPa.left = this;
+			}
+
+
+		} else  {
+			this.left = parent.left;
+			this.right = parent;
+
+			if (parent.left) {
+				parent.left.parent = this;
+			}
+
+			parent.left = leftC;
+			parent.right = rightC;
+
+			if (grandPa && grandPa.right == parent) {
+				grandPa.right = this;
+			} else if (grandPa && grandPa.left == parent) {
+				grandPa.left = this;
+			}
+		}
+	    // if (this.left) {
+		// 	leftC.parent = parent;
+		// 	this.parent.left = leftC;
+		// }
+	    // if (this.right) {
+		// 	rightC.parent = parent;
+		// 	this.parent.right = rightC;
+		// }
+
+
+
+
 
 	}
 }
 
 module.exports = Node;
 
-let a = new Node (1, 15);
-let b = new Node(2, 10);
-let c = new Node(3, 20);
-b.parent = a;
-c.parent = a;
-
-a.appendChild(b);
-a.appendChild(c);
-b.swapWithParent();
-console.log(b);
+// let a = new Node (1, 15);
+// let b = new Node(2, 10);
+// let c = new Node(3, 20);
+// b.parent = a;
+// c.parent = a;
+//
+// a.appendChild(b);
+// a.appendChild(c);
+// b.swapWithParent();
+// console.log(b);
